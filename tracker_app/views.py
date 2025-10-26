4c1beb7eb045ffb75067a6a9a0ddfff0e5b4571f
rushikesh <rushikeshphatak04@gmail.com>
2025-10-26 20:48:42 +0530

    final deployment

--- tracker_app/views.py
+++ tracker_app/views.py
@@ -25,7 +25,7 @@ from .serializers import GeofenceSerializer
 
 # To convert a date string to an object
 from django.utils.dateparse import parse_date
-import os
+
 
 # Create your views here.
 
@@ -217,21 +217,3 @@ def admin_dashboard_view(request):
 
 def home_view(request):
     return render(request, 'tracker_app/home.html')
-
-
-def create_superuser_temp(request):
-    from django.contrib.auth.models import User
-
-    # तुमच्या एन्व्हायर्नमेंट व्हेरिएबल्समधून युजरनेम आणि पासवर्ड घ्या
-    # हे Render च्या Environment टॅबमध्ये सेट करा
-    username = os.environ.get('ADMIN_USER', 'admin')
-    password = os.environ.get('ADMIN_PASS', 'a_strong_password')
-    email = os.environ.get('ADMIN_EMAIL', 'admin@example.com')
-
-    if not User.objects.filter(username=username).exists():
-        print(f"Creating superuser: {username}")
-        User.objects.create_superuser(
-            username=username, email=email, password=password)
-        return HttpResponse(f"Superuser '{username}' created successfully!")
-    else:
-        return HttpResponse(f"Superuser '{username}' already exists.")
